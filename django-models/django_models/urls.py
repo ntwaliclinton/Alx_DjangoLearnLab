1b46c59bdbd80b41fdaa6808103339aa6c070b4f
ntwaliclinton <ntwalibillclinton@gmail.com>
2024-08-17 15:16:57 +0200

    2. Implementing User Authentication in Django

--- django-models/django_models/urls.py
+++ django-models/django_models/urls.py
@@ -9,3 +9,10 @@ urlpatterns = [
     path('', home, name='home'),  # This is the root URL
     path('relationship_app/', include('relationship_app.urls')),
 ]
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('relationship_app/', include('relationship_app.urls')),
+]
