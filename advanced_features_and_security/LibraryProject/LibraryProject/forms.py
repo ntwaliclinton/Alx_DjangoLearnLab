41c08c4706b0f1990717d263218f01f69e68ca61
ntwaliclinton <ntwalibillclinton@gmail.com>
2024-08-25 22:20:49 +0200

    2. Implementing Security Best Practices in Django

---
+++ advanced_features_and_security/LibraryProject/LibraryProject/forms.py
@@ -0,0 +1,6 @@
+# LibraryProject/bookshelf/forms.py
+from django import forms
+
+class ExampleForm(forms.Form):
+    name = forms.CharField(label='Name', max_length=100)
+    email = forms.EmailField(label='Email')
