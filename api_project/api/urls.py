238e456228ca7a4cdf1ac3b473c90a58f25a96a6
ntwaliclinton <ntwalibillclinton@gmail.com>
2024-08-29 14:37:16 +0200

    1. Building Your First API Endpoint with Django REST Framework

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from .views import BookList
+serializers.ModelSerializer
+from rest_framework import serializers
 
 urlpatterns = [
     path('api/books/', BookList.as_view(), name='book_list'),
