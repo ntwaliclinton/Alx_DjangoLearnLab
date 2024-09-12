1d3c85f669c04ff4ce4883c9ddced8b6e3a84e33
ntwaliclinton <ntwalibillclinton@gmail.com>
2024-09-12 14:23:48 +0200

    2. Creating Blog Post Management Features

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('post/<int:pk>/update/'),
 ]
