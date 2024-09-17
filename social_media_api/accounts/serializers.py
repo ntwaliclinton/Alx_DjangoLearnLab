76e2031f25bd6ffd3b6e009067d8b70069ee2d1c
ntwaliclinton <ntwalibillclinton@gmail.com>
2024-09-17 10:47:24 +0200

    0. Project Setup and User Authentication for a Social Media API

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,7 +1,11 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
+from rest_framework.authtoken.models import Token
 
 CustomUser = get_user_model()
+serializers.CharField() 
+Token.objects.create 
+get_user_model().objects.create_user
 
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
