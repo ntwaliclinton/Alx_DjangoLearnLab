550f71f4687c3018be967ce02396f68936f4096e
ntwaliclinton <ntwalibillclinton@gmail.com>
2024-09-22 21:39:40 +0200

    3. Implementing Notifications and Likes Functionality

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -6,6 +6,14 @@ from rest_framework.permissions import IsAuthenticated
 from .models import Post
 from accounts.models import CustomUser
 from .serializers import PostSerializer
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.shortcuts import get_object_or_404
+from .models import Post, Like
+from notifications.models import Notification
+from django.contrib.contenttypes.models import ContentType
 
 ["permissions.IsAuthenticated"]
 
@@ -26,14 +34,6 @@ class FeedView(generics.ListAPIView):
         following_users = user.following.all()  # Users that the current user follows
         return Post.objects.filter(author__in=following_users).order_by('-created_at')
     
-    from rest_framework import status
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from django.shortcuts import get_object_or_404
-from .models import Post, Like
-from notifications.models import Notification
-from django.contrib.contenttypes.models import ContentType
 
 class LikePostView(APIView):
     permission_classes = [IsAuthenticated]
